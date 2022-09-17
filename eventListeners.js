89e544fd53dd8f25fa8deb5c27d8194d23136c55
jmadrigaladan <jorgemadrigaladan@gmail.com>
2022-09-17 16:40:13 -0700

    navbar is now responsive

---
+++ eventListeners.js
@@ -0,0 +1,15 @@
+const navSlide = () => {
+  const phoneNavBar = document.querySelector(".phone__navbar");
+  const phoneMenu = document.querySelector(".phone__menu");
+  const bentoMenu = document.querySelector(".bento-menu");
+  const closeBtnMenu = document.querySelector(".close__btn-menu");
+
+  phoneNavBar.addEventListener("click", () => {
+    phoneMenu.classList.toggle("phone__menu--active");
+    bentoMenu.classList.toggle("bento-menu--active");
+    closeBtnMenu.classList.toggle("close__btn-menu--active");
+  });
+};
+
+navSlide();
+console.log("eventlistener.js");
